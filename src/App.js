74c239c196764e4884332239793bf95b74f4b1ab
Sam Khalandovsky <samkhal@gmail.com>
2022-05-07 15:35:54 -0700

    Implement promotion interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react"
-import ChessgroundSovereign from './ChessgroundSovereign';
-import { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import { useState, useEffect, useRef } from 'react';
 import createModule from "./engine.mjs"
+import PromotionDialog from './PromotionDialog';
+import { Chessground as NativeChessground } from 'chessground-sovereign'
+
+import "./assets/theme.css"
+import "./assets/examples.css"
+import "./assets/chessground.css"
 
 function movelistToDests(movelist) {
   var dests = new Map();
@@ -17,13 +23,18 @@ function movelistToDests(movelist) {
   }
   return dests;
 }
-
+export const key2pos = (k) => [k.charCodeAt(0) - 97, k.charCodeAt(1) < 58 ? k.charCodeAt(1) - 49 : k.charCodeAt(1) - 65 + 9];
 
 const initialFen = 'aqabvrvnbrbnbbbqbkbbbnbrynyrsbsq/aranvpvpbpbpbpbpbpbpbpbpypypsnsr/nbnp12opob/nqnp12opoq/crcp12rprr/cncp12rprn/gbgp12pppb/gqgp12pppq/yqyp12vpvq/ybyp12vpvb/onop12npnn/orop12npnr/rqrp12cpcq/rbrp12cpcb/srsnppppwpwpwpwpwpwpwpwpgpgpanar/sqsbprpnwrwnwbwqwkwbwnwrgngrabaq w';
 function App() {
   const [getLegalMoves, setGetLegalMoves] = useState(null);
   const [fen, setFen] = useState(initialFen);
   const [turnPlayer, setTurnPlayer] = useState("white");
+  const [pendingMove, setPendingMove] = useState();
+
+  const cg = useRef(null);
+
+  const [promotionDialogColor, setPromotionDialogColor] = useState(undefined);
 
   useEffect(() => {
     createModule().then((Module) => {
@@ -31,45 +42,84 @@ function App() {
     });
   }, []);
 
-  function handleMove(_orig, _dest, _metadata, fen) {
+  function handleMove(orig, dest) {
 
-    let newTurnPlayer = turnPlayer === 'white' ? 'black' : 'white';
-    setTurnPlayer(newTurnPlayer);
+    // is this a promotion?
+    const destPos = key2pos(dest);
+    const piece = cg.current.state.pieces.get(dest);
+    if (piece.role === 'pawn' &&
+      destPos[0] > 5 && destPos[0] < 10 &&
+      destPos[1] > 5 && destPos[1] < 10) {
+      setPendingMove({
+        from: orig,
+        to: dest,
+        color: piece.color
+      });
+      setPromotionDialogColor(piece.color);
+    }
+    else {
 
-    let new_fen = fen;
-    new_fen += " ";
-    new_fen += newTurnPlayer === 'white' ? 'w' : 'b';
-    setFen(new_fen);
-    setTurnPlayer(newTurnPlayer);
+      let newTurnPlayer = turnPlayer === 'white' ? 'black' : 'white';
+      setTurnPlayer(newTurnPlayer);
+
+      let new_fen = cg.current.getFen();
+      new_fen += " ";
+      new_fen += newTurnPlayer === 'white' ? 'w' : 'b';
+      setFen(new_fen);
+      setTurnPlayer(newTurnPlayer);
+    }
   }
 
   if (!getLegalMoves) {
     return "Loading..."; // TODO(samkhal)
   }
 
+  const config = {
+    fen: fen,
+    turnPlayer: turnPlayer,
+    premovable: {
+      enabled: false
+    },
+    drawable: {
+      enabled: false
+    },
+    movable: {
+      color: 'both',
+      free: false,
+      dests: movelistToDests(getLegalMoves(fen)),
+      events: { after: handleMove }
+    },
+    highlight: {
+      lastMove: false,
+    }
+  };
+
+  if (cg.current) {
+    cg.current.set(config);
+  }
+
+  function setCgElement(el) {
+    if (cg.current) {
+      return;
+    }
+    if (el)
+      cg.current = NativeChessground(el, config);
+  }
+
+  function handlePromotionSelection(selectedRole) {
+    cg.current.setPieces(new Map([
+      [pendingMove.from, undefined],
+      [pendingMove.to, { role: selectedRole, color: pendingMove.color, promoted: true }]
+    ]));
+    setPromotionDialogColor(undefined);
+    handleMove(pendingMove.from, pendingMove.to);
+  }
+
+
   return (
     <div className="App">
-      <ChessgroundSovereign
-        config={{
-          fen: fen,
-          turnPlayer: turnPlayer,
-          premovable: {
-            enabled: false
-          },
-          drawable: {
-            enabled: false
-          },
-          movable: {
-            color: 'both',
-            free: false,
-            dests: movelistToDests(getLegalMoves(fen)),
-            events: { after: handleMove }
-          },
-          highlight: {
-            lastMove: false,
-          }
-        }}
-      />
+      <div ref={el => setCgElement(el)} />
+      <PromotionDialog color={promotionDialogColor} onClick={handlePromotionSelection} />
     </div>
   );
 }
---
+++ src/PromotionDialog.js
@@ -0,0 +1,42 @@
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+function PieceImage(props) {
+  const pieceName = props.color + " " + props.role;
+
+  return (
+    <piece className={pieceName + " promotion"}>
+      <svg onClick={() => props.onClick(props.role)} viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+        <use className={pieceName} href={'#' + props.role + '-svg'} />
+      </svg>
+    </piece>
+  );
+}
+
+export default function PromotionDialog(props) {
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  return (
+    <Modal open={props.color !== undefined}>
+      <Box sx={style}>
+        <span>
+          <PieceImage role="knight" color={props.color} onClick={props.onClick} />
+          <PieceImage role="bishop" color={props.color} onClick={props.onClick} />
+          <PieceImage role="rook" color={props.color} onClick={props.onClick} />
+          <PieceImage role="queen" color={props.color} onClick={props.onClick} />
+        </span>
+      </Box>
+    </Modal>
+  );
+}
